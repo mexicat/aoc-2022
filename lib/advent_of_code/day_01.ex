d230de9e69952b36792fb11ab8b012d2566c942d
Giacomo Magnanini <gikappa@gmail.com>
2022-12-01 07:50:30 +0100

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,9 +1,15 @@
 defmodule AdventOfCode.Day01 do
   def part1(input) do
-    input
+    input |> parse_input() |> Enum.map(&Enum.sum/1) |> Enum.max()
   end
 
   def part2(input) do
+    input |> parse_input() |> Enum.map(&Enum.sum/1) |> Enum.sort() |> Enum.take(-3) |> Enum.sum()
+  end
+
+  def parse_input(input) do
     input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn elf -> elf |> String.split() |> Enum.map(&String.to_integer/1) end)
   end
 end
